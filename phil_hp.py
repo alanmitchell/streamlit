3e35f075c492d54416ee03acec61a1cf60fa0523
Alan Mitchell <tabb99@gmail.com>
2020-09-02 18:39:05 -0800

    Improved Hover text and dot size related to recency.

--- phil_hp.py
+++ phil_hp.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas as pd
+import numpy as np
 import plotly.express as px
 import bmondata
 
@@ -13,7 +14,7 @@ def run():
     refresh = streamlit.sidebar.button('Refresh Data')
     if dfr is None or refresh:
         server = bmondata.Server('https://bmon.analysisnorth.com')
-        start_ts = '2020-09-02'
+        start_ts = '2020-09-01'
         df = server.sensor_readings([
                 ('phil_hp_out_13_btu_heat', 'heat'),
                 ('phil_hp_out_13_btu_thot', 'leaving_t'),
@@ -47,19 +48,65 @@ def run():
             rec['heat_dt'] = rec['leaving_t'] - rec['entering_t']
             rec['entering_out_dt'] = rec['entering_t'] - rec['out_t']
             rec['date_time'] =  pd.to_datetime((cycle_en - cycle_st) / 2.0 + cycle_st)
-            rec['date_time_str'] = rec['date_time'].strftime('%D %H:%M:%S')
-            rec['cycle_minutes'] = (cycle_en - cycle_st) / 1e9 / 60.0
+            rec['date_time_str'] = rec['date_time'].strftime('%-m/%-d/%y %-I:%M %p')
+            rec['cycle_minutes'] = float((cycle_en - cycle_st)) / 1e9 / 60.0
             recs.append(rec)
 
         dfr = pd.DataFrame(recs)
 
     streamlit.markdown("""## COP vs. Outdoor Temperature
 
-Each point is one cycle.  The color of the point indicates the compressor power during
-the cycle.
+Each point is one cycle, and the COP shown only includes energy flows during the cycle.
+The color of the point indicates the compressor power during
+the cycle.  Larger dots are more recent cycles.
 """)
+
+    # make a size column that allows for enlarging recent points
+    size = np.ones(len(dfr))
+    sz = 0.5
+    for i in range(len(size)):
+        size[-1-i] = sz
+        sz -= 0.1
+        if sz < 0.05:
+            sz = 0.05
+    dfr['size'] = size
+
+    # Make arrays to be used for creating custom hover text
+    out_t = dfr.out_t.values
+    entering_out_dt = dfr.entering_out_dt.values
+    heat = dfr.heat.values
+    flow = dfr.flow.values
+    entering_t = dfr.entering_t.values
+    heat_dt = dfr.heat_dt.values
+    date_time_str = dfr.date_time_str.values
+    cycle_minutes = dfr.cycle_minutes.values
+
+    customdata = np.dstack((
+        date_time_str,      # 0
+        heat,               # 1     
+        out_t,              # 2
+        entering_out_dt,    # 3
+        flow,               # 4
+        entering_t,         # 5
+        heat_dt,            # 6
+        cycle_minutes,      # 7
+    ))[0]
+
+    # Template for the hover text:
+    hover_tmpl = """%{customdata[0]} <br>
+Outdoor Temperature: %{x:.1f} F <br>
+COP: %{y:.2f} <br>
+Power: %{marker.color:,.0f} W <br>
+Heat Output: %{customdata[1]:,.0f} BTU/hour <br>
+Entering - Outside Temp: %{customdata[3]:.1f} F <br>
+Flow: %{customdata[4]:.1f} gpm <br>
+HP Entering Temp: %{customdata[5]:.1f} F <br>
+HP Delta Temp: %{customdata[6]:.1f} F <br>
+Cycle Length: %{customdata[7]:,.0f} minutes
+"""
+
     fig = px.scatter(dfr, x='out_t', y='cop', 
-        color='power', 
+        color='power', size='size',
         hover_data=['date_time_str', 'entering_t', 'flow', 'heat_dt', 'cycle_minutes'], 
         color_continuous_scale="Bluered_r")
     fig.update_layout(
@@ -67,12 +114,29 @@ the cycle.
         yaxis_title = 'COP',
         coloraxis_colorbar_title_text = 'Compressor Power'
     )
-    streamlit.write(fig)
+    fig.update_traces(
+        customdata=customdata,
+        hovertemplate=hover_tmpl,
+    )
+    streamlit.plotly_chart(fig, use_container_width=True)
 
     streamlit.markdown('## COP vs. (Heat Pump Entering - Outdoor Temp')
 
+    # Template for the hover text:
+    hover_tmpl = """%{customdata[0]} <br>
+Outdoor Temperature: %{customdata[2]:.1f} F <br>
+COP: %{y:.2f} <br>
+Power: %{marker.color:,.0f} W <br>
+Heat Output: %{customdata[1]:,.0f} BTU/hour <br>
+Entering - Outside Temp: %{x:.1f} F <br>
+Flow: %{customdata[4]:.1f} gpm <br>
+HP Entering Temp: %{customdata[5]:.1f} F <br>
+HP Delta Temp: %{customdata[6]:.1f} F <br>
+Cycle Length: %{customdata[7]:,.0f} minutes
+"""
+
     fig = px.scatter(dfr, x='entering_out_dt', y='cop', 
-        color='power', 
+        color='power', size='size', 
         hover_data=['date_time_str', 'entering_t', 'out_t', 'flow', 'heat_dt', 'cycle_minutes'], 
         color_continuous_scale="Bluered_r")
     fig.update_layout(
@@ -80,4 +144,11 @@ the cycle.
         yaxis_title = 'COP',
         coloraxis_colorbar_title_text = 'Compressor Power'
     )
-    streamlit.write(fig)
+    fig.update_traces(
+        customdata=customdata,
+        hovertemplate=hover_tmpl,
+    )
+    streamlit.plotly_chart(fig, use_container_width=True)
+
+    streamlit.markdown('## Raw Data')
+    streamlit.write(dfr)
