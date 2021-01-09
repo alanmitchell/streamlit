d9b77495a1c7cc79c0bb8a331ff061c13d74ae89
Alan Mitchell <tabb99@gmail.com>
2021-01-09 10:40:12 -0900

    Labeled Phil's TIG.

--- lora_sig.py
+++ lora_sig.py
@@ -25,7 +25,8 @@ gtw_lbls = {
     'eui-000080029c2b2b77': 'Bear Valley',
     'eui-a84041ffff1ee2b4': 'Dragino Outdoor',
     'eui-a840411eed744150': 'Phil Home',
-    'eui-58a0cbfffe802c68': "Altermatt TIG"
+    'eui-58a0cbfffe802c68': "Altermatt TIG",
+    'eui-58a0cbfffe80326e': 'Phil TIG',
 }
 
 dev_lbls = dict(
