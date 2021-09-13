ed5785cacbf94a40b4fb8aaee6575f15f0ef6477
Alan Mitchell <tabb99@gmail.com>
2021-09-13 15:57:10 -0800

    Used temporary directory to hold extract file.

--- lora_sig.py
+++ lora_sig.py
@@ -2,6 +2,7 @@
 """
 import time
 from pathlib import Path
+from tempfile import TemporaryDirectory
 from dateutil.parser import parse
 import pytz
 import streamlit as st
@@ -25,12 +26,13 @@ def dev_map(x):
 # timezone of the 'ts' column in the data is Alaska
 tz_data = pytz.timezone('US/Alaska')
 
-def get_readings(sensor, data_file=lora_data_file):
-    """Returns a DataFrame of the readings for the sensor (a sensor label) starting at the
+def get_readings(sensor, temp_dir_name, data_file=lora_data_file):
+    """Returns a DataFrame of the readings for the 'sensor' (a sensor label) starting at the
     beginning of yesterday. Readings are read from the file with a full path 
     of 'data_file'.  Only the Gateway, SNR, data_rate, counter and the ts (timestamp) columns are retained, 
     and the timestamp column is converted to a DateTime column.  Gateway labels are used
-    if available for a particular Gateway ID.
+    if available for a particular Gateway ID.  The function uses the directory 'temp_dir_name'
+    to hold the extract from the data file.
     """
     # Filter to desired sensor and desired columns
     # First need to convert the sensor label into a Dev ID.  Need to reverse
@@ -40,14 +42,15 @@ def get_readings(sensor, data_file=lora_data_file):
 
     # create a file in this directory with rows starting the beginning of yesterday,
     # filtered down to the target dev_id.
+    extract_file = Path(temp_dir_name) / 'gtw.tsv'
     try:
-        trim_file(data_file, './gtw.tsv', 1, dev_id)
+        trim_file(data_file, extract_file, 1, dev_id)
     except:
         # were no records, just the header row
         pass
 
     # read it into a DataFrame    
-    df = pd.read_csv('./gtw.tsv', sep='\t')
+    df = pd.read_csv(extract_file, sep='\t')
 
     # pull just the needed columns
     df = df[['ts', 'gateway', 'snr', 'data_rate', 'counter']]
@@ -86,55 +89,56 @@ def run():
         file_mod_time = None
         last_ts = None
         last_datarate = None
-        while (time.time() - start)/60.0 < rcv_time:
+        with TemporaryDirectory() as tempdirname:
+            while (time.time() - start)/60.0 < rcv_time:
 
-            if last_ts:
-                seconds_ago = time.time() - last_ts
-                txt_seconds_ago.markdown(f'### {seconds_ago:,.0f} secs ago, {last_datarate}')
+                if last_ts:
+                    seconds_ago = time.time() - last_ts
+                    txt_seconds_ago.markdown(f'### {seconds_ago:,.0f} secs ago, {last_datarate}')
 
-            # only re-read the file if it has changed
-            cur_file_mod_time = Path(lora_data_file).stat().st_mtime
-            if file_mod_time != cur_file_mod_time:
-                df = get_readings(sensor)
-                file_mod_time = cur_file_mod_time
+                # only re-read the file if it has changed
+                cur_file_mod_time = Path(lora_data_file).stat().st_mtime
+                if file_mod_time != cur_file_mod_time:
+                    df = get_readings(sensor, tempdirname)
+                    file_mod_time = cur_file_mod_time
 
-            if len(df):
-                df_rdg = df.groupby('counter')
-                df_last = list(df_rdg)[-1][1]             # DataFrame for last reading
+                if len(df):
+                    df_rdg = df.groupby('counter')
+                    df_last = list(df_rdg)[-1][1]             # DataFrame for last reading
 
-                # get the first gateway in order to extract timestamp and data rate.
-                first_gtw = df_last.iloc[0].to_dict()
+                    # get the first gateway in order to extract timestamp and data rate.
+                    first_gtw = df_last.iloc[0].to_dict()
 
-                # convert the date/time into UTC and get a Unix timestamp from it
-                last_ts = tz_data.localize(first_gtw['Time']).astimezone(pytz.UTC).timestamp()
-                last_datarate = first_gtw['data_rate']
-                seconds_ago = time.time() - last_ts
-                txt_seconds_ago.markdown(f'### {seconds_ago:,.0f} secs ago, {last_datarate}')
+                    # convert the date/time into UTC and get a Unix timestamp from it
+                    last_ts = tz_data.localize(first_gtw['Time']).astimezone(pytz.UTC).timestamp()
+                    last_datarate = first_gtw['data_rate']
+                    seconds_ago = time.time() - last_ts
+                    txt_seconds_ago.markdown(f'### {seconds_ago:,.0f} secs ago, {last_datarate}')
 
-                # Use the Dataframe of the last reading to make the top plot
-                df_last['SNR above -10 dB'] = df_last.SNR + 10
-                df_last.sort_values('Gateway', inplace=True) 
-                fig = px.bar(df_last, x='Gateway', y='SNR above -10 dB')  
-                fig.update_yaxes(range=[0, 20])
-                fig.update_xaxes(
-                    tickangle = 30,
-                    title_font = {'size': 15},
-                    tickfont = {'size': 15},
-                )
-                cht.plotly_chart(fig, use_container_width=True)
+                    # Use the Dataframe of the last reading to make the top plot
+                    df_last['SNR above -10 dB'] = df_last.SNR + 10
+                    df_last.sort_values('Gateway', inplace=True) 
+                    fig = px.bar(df_last, x='Gateway', y='SNR above -10 dB')  
+                    fig.update_yaxes(range=[0, 20])
+                    fig.update_xaxes(
+                        tickangle = 30,
+                        title_font = {'size': 15},
+                        tickfont = {'size': 15},
+                    )
+                    cht.plotly_chart(fig, use_container_width=True)
 
-                # Plot a scatter plot of all readings, all gateways.
-                fig = px.scatter(df, x='Time', y='SNR', color='Gateway')
-                fig.update_yaxes(range=[-15, 15])
-                cht_history.plotly_chart(fig, use_container_width=True)
-            
-            else:
-                txt_seconds_ago.markdown('No Recent Sensor Readings')
-                cht.empty()
-                cht_history.empty()
+                    # Plot a scatter plot of all readings, all gateways.
+                    fig = px.scatter(df, x='Time', y='SNR', color='Gateway')
+                    fig.update_yaxes(range=[-15, 15])
+                    cht_history.plotly_chart(fig, use_container_width=True)
+                
+                else:
+                    txt_seconds_ago.markdown('No Recent Sensor Readings')
+                    cht.empty()
+                    cht_history.empty()
 
-            time.sleep(2)
+                time.sleep(2)
 
-        txt_seconds_ago.empty()
-        cht.empty()
-        cht_history.empty()
+            txt_seconds_ago.empty()
+            cht.empty()
+            cht_history.empty()
