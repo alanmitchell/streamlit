c1874abc63bf217d6d5a80618826c10f03be462e
Alan Mitchell <tabb99@gmail.com>
2021-09-26 10:50:39 -0800

    Correct Phil ELT-2 label.

--- label_map.py
+++ label_map.py
@@ -32,7 +32,7 @@ dev_id_lbls = {
     'boat-lt2-a8404137b182428e': 'Phil LT22222 428E',
     'ers-a81758fffe04259e': 'Alan ERS 259E',
     'ersco2-a81758fffe0526d8': 'Phil CO2 26D8',
-    'elt-2_a81758fffe053692': 'Phil ELT-2 3692',
+    'elt-2-a81758fffe053692': 'Phil ELT-2 3692',
     'lht65-a8404173e1822ca4': 'Alan LHT65 2CA4',
     'boat-lt2-a84041552182436a': 'Audacity Boat 436A',
     'lht65-a8404175d1822ca7': 'Peter LHT65 2CA7',
