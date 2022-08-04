3e7cc17f2202740eb4144b68e8f1e55fd60e71ca
Alan Mitchell <tabb99@gmail.com>
2022-08-03 22:06:01 -0800

    Finished device filter.

--- pages/10_LoRa_Sensor_Diagnostics.py
+++ pages/10_LoRa_Sensor_Diagnostics.py
@@ -19,4 +19,21 @@ def all_devices():
     devices.sort()
     return devices
 
-st.sidebar.selectbox('Select a Device', all_devices())
+def filter_devices():
+    st.session_state.filtered_devices = [d for d in all_devices() if st.session_state.filter in d]
+    if len(st.session_state.filtered_devices) > 1:
+        # insert a blank item at the top so no real device is selected
+        st.session_state.filtered_devices.insert(0, '')
+
+st.sidebar.text_input('Text to Filter Devices', key='filter', on_change=filter_devices)
+
+# deal with case where the filter_device() function has never run during the session
+if 'filtered_devices' not in st.session_state:
+    st.session_state.filtered_devices = []
+
+device = st.sidebar.selectbox('Select a Device', st.session_state.filtered_devices)
+if not device:
+    st.write('No Device selected')
+    st.stop()
+
+st.write(f'**Device ID:** {device}')
